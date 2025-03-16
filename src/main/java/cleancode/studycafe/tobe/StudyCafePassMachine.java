59e9ff7a3bd144bc23a082024ee316bd20bffff7
Minsu Kim <gts2044@gmail.com>
2025-03-16 15:06:58 +0900

    리팩토링(3) - FileHandler를 Provider 개념으로 리팩토링

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import cleancode.studycafe.tobe.exception.AppException;
-import cleancode.studycafe.tobe.io.StudyCafeFileHandler;
 import cleancode.studycafe.tobe.io.StudyCafeIOHandler;
 import cleancode.studycafe.tobe.model.order.StudyCafePassOrder;
 import cleancode.studycafe.tobe.model.pass.StudyCafePassType;
@@ -12,16 +11,24 @@ import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPass;
 import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPasses;
 import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
 import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPasses;
+import cleancode.studycafe.tobe.provider.LockerPassProvider;
+import cleancode.studycafe.tobe.provider.SeatPassProvider;
 
 public class StudyCafePassMachine {
 
 	private final StudyCafeIOHandler ioHandler = new StudyCafeIOHandler();
-	private final StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
+	private final SeatPassProvider seatPassProvider;
+	private final LockerPassProvider lockerPassProvider;
 
-    public void run() {
-        try {
-            ioHandler.showWelcomeMessage();
-            ioHandler.showAnnouncement();
+	public StudyCafePassMachine(SeatPassProvider seatPassProvider, LockerPassProvider lockerPassProvider) {
+		this.seatPassProvider = seatPassProvider;
+		this.lockerPassProvider = lockerPassProvider;
+	}
+
+	public void run() {
+		try {
+			ioHandler.showWelcomeMessage();
+			ioHandler.showAnnouncement();
 
 			StudyCafeSeatPass selectedPass = selectPass();
 			Optional<StudyCafeLockerPass> optionalLockerPass = selectLockerPass(selectedPass);
@@ -31,12 +38,12 @@ public class StudyCafePassMachine {
 			);
 
 			ioHandler.showPassOrderSummary(passOrder);
-        } catch (AppException e) {
-            ioHandler.showSimpleMessage(e.getMessage());
-        } catch (Exception e) {
-            ioHandler.showSimpleMessage("알 수 없는 오류가 발생했습니다.");
-        }
-    }
+		} catch (AppException e) {
+			ioHandler.showSimpleMessage(e.getMessage());
+		} catch (Exception e) {
+			ioHandler.showSimpleMessage("알 수 없는 오류가 발생했습니다.");
+		}
+	}
 
 	private StudyCafeSeatPass selectPass() {
 		StudyCafePassType passType = ioHandler.askPassTypeSelecting();
@@ -46,7 +53,7 @@ public class StudyCafePassMachine {
 	}
 
 	private List<StudyCafeSeatPass> findPassCandidatesBy(StudyCafePassType studyCafePassType) {
-		StudyCafeSeatPasses allPasses = studyCafeFileHandler.readStudyCafePasses();
+		StudyCafeSeatPasses allPasses = seatPassProvider.getSeatPasses();
 		return allPasses.findPassBy(studyCafePassType);
 	}
 
@@ -70,7 +77,7 @@ public class StudyCafePassMachine {
 	}
 
 	private Optional<StudyCafeLockerPass> findLockerPassCandidateBy(StudyCafeSeatPass pass) {
-		StudyCafeLockerPasses allLockerPasses = studyCafeFileHandler.readLockerPasses();
+		StudyCafeLockerPasses allLockerPasses = lockerPassProvider.getLockerPasses();
 		return allLockerPasses.findLockerPassBy(pass);
 	}
 
